0fc40e04be2a9e7a0924fd8f5baa21de22db1d9b
Acer <Acer@Acer-PC>
2020-01-03 18:09:57 +0530

    INHERITANCE: 	In this application create the four classes and four seperate files.

---
+++ src/inheritance/LavaDino.java
@@ -0,0 +1,31 @@
+package inheritance;
+
+public class LavaDino {
+	protected String species;
+	//protected String species1;
+
+	public LavaDino(String species) {
+		super();
+		this.species = species;
+		//this.species1 = species1;
+	}
+	
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	/*public String getSpecies1() {
+		return species1;
+	}
+
+	public void setSpecies1(String species1) {
+		this.species1 = species1;
+	}
+	*/
+	
+}
